fa1d43dda7b363983dbc53d42ae7601e2fe6e8f7
dario-c <dariocavanillas@gmail.com>
2014-08-11 10:36:53 +0200

    refactor

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,4 @@
 class Project < ActiveRecord::Base
   has_many :tasks
-
-
   validates :name, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,11 @@
 class Task < ActiveRecord::Base
-  
+  belongs_to :project  
   after_initialize :init
 
   def init
     self.status ||= "todo"
   end
 
-
-  belongs_to :project
   validates :name, presence: true
   validates :status, inclusion: { in: %w(todo doing done),
     message: "%{value} is not a valid status"}
